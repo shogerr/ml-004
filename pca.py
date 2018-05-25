cb782d702e22fa0599de8cd131113b7960d03ff0
Alex Ruef <ruefa@oregonstate.edu>
2018-05-24 19:33:17 -0700

    calculated mean

--- pca.py
+++ pca.py
@@ -1,5 +1,10 @@
 import numpy as np
 
 class pca:
+	# data_path: string of data file path
 	def __init__(self, data_path):
+		# load data from file
 		self.data = np.loadtxt(data_path, dtype=np.int, delimiter=',')
+
+		# calculate mean
+		self.mean = np.mean(self.data)
