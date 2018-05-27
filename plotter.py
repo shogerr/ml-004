f4de3fa8c1494325e7138613e50df72959bf8a54
ruefa <ruef.alex@gmail.com>
2018-05-27 13:08:57 -0700

    added method to plot pca images in seperate file

---
+++ plotter.py
@@ -0,0 +1,25 @@
+import numpy as np
+from matplotlib import pyplot
+
+def pca_plotter(pca):
+	# draw top 10 eigen vecs
+	#for vec in pca.top_eigen_vecs:
+		#scaled_vec = [x/max(vec) for x in vec]
+		#pyplot.imshow(np.reshape(scaled_vec,(28,28)))
+		#pyplot.show()
+		#input("next")
+
+	# draw mean vector
+	#pyplot.imshow(np.reshape(pca.mean,(28,28)))
+	#pyplot.show()
+
+	# draw images reduced by eigenvectors
+	# only draws images with highest value
+	# for one of the ten dimensions
+	for vec in pca.highest_dim_vecs:
+		pyplot.imshow(np.reshape(np.dot(pca.top_eigen_vecs, vec),(5,2)))
+		pyplot.show()
+		input("next")
+		pyplot.imshow(np.reshape(vec,(28,28)))
+		pyplot.show()
+		input("next")
